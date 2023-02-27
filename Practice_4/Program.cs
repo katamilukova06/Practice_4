47c296c2bbd45ce24ca3fc25f5002ffcfccf4cd9
katamilukova06 <122910788+katamilukova06@users.noreply.github.com>
2023-02-27 15:34:05 +0200

    add

---
+++ Practice_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 2
+            //int[] omas = new int[] { -1, 0, 1, 2, 3 };
+            //long sum = 0;
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    sum += omas[i];
+            //}
+            //Console.WriteLine($"Сумма={sum}");
+            //Console.ReadKey();
+
+            //Задание 3
+            //int[] omas = new int[] {-1, 2, 3, 4, 5 };
+            //int max= omas[0];
+            //for (int i=0; i<omas.Length; i++)
+            //{
+            //    if (omas[i] > max)
+            //    {
+            //        max = omas[i];
+            //    }
+            //}
+            //Console.WriteLine($"max={max}");
+            //int p = 0;
+            //p = omas[0];
+            //omas[0] = max;
+            //max = p;
+            //for (int i=0; i<omas.Length;i++)
+            //{
+            //    Console.Write($"{omas[i]}");
+            //}
+            //Console.ReadKey();
+
+        }
+    }
+}
